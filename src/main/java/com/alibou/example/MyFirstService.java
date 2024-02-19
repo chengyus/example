1ec9bf3442186c2eed91456e64047720a4e4a951
Joseph Sung <chengyu.sung@gmail.com>
2024-02-19 15:07:02 +0800

    changes to sync around 1:28:03

--- src/main/java/com/alibou/example/MyFirstService.java
+++ src/main/java/com/alibou/example/MyFirstService.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyFirstService {
 
-  @Autowired
-  @Qualifier("bean1")
   private MyFirstClass myFirstClass;
+
+  @Autowired
+  public void injectDependencies(@Qualifier("bean1") MyFirstClass myFirstClass) {
+    this.myFirstClass = myFirstClass;
+  }
 
   public String tellAStory() {
     return "the dependency is saying : " + myFirstClass.sayHello();
