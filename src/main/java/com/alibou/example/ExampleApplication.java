bf0fad16e0041c961cfd636ede5ea102b777e3e6
Joseph Sung <chengyu.sung@gmail.com>
2024-02-20 13:15:23 +0800

    re-indent

--- src/main/java/com/alibou/example/ExampleApplication.java
+++ src/main/java/com/alibou/example/ExampleApplication.java
@@ -13,7 +13,7 @@ public class ExampleApplication {
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellAStory());
 		System.out.println(myFirstService.getJavaVersion());
-    System.out.println(myFirstService.getOsName());
+		System.out.println(myFirstService.getOsName());
 		System.out.println(myFirstService.readProp());
 	}
 
