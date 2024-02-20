96cf9da3567bddb60d6014f71d320ebb87c0c9e0
Joseph Sung <chengyu.sung@gmail.com>
2024-02-20 16:55:00 +0800

    custom.properties and output consistent with video around 1:36:48.

--- src/main/java/com/alibou/example/ExampleApplication.java
+++ src/main/java/com/alibou/example/ExampleApplication.java
@@ -12,6 +12,10 @@ public class ExampleApplication {
 
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellAStory());
+		System.out.println(myFirstService.getCustomProperty());
+		System.out.println(myFirstService.getCustomPropertyInt());
+		System.out.println(myFirstService.getGetCustomPropertyFromAnotherFile());
+
 	}
 
 }
--- src/main/java/com/alibou/example/MyFirstService.java
+++ src/main/java/com/alibou/example/MyFirstService.java
@@ -2,12 +2,21 @@ package com.alibou.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
+@PropertySource("classpath:custom.properties")
 public class MyFirstService {
 
   private final MyFirstClass myFirstClass;
+  @Value("Hello Alibou students")
+  private String customProperty;
+  @Value("${my.prop}")
+  private String customPropertyFromAnotherFile;
+  @Value("123")
+  private Integer customerPropertyInt;
 
   public MyFirstService(@Qualifier("bean1") MyFirstClass myFirstClass) {
     this.myFirstClass = myFirstClass;
@@ -17,4 +26,15 @@ public class MyFirstService {
     return "the dependency is saying : " + myFirstClass.sayHello();
   }
 
+  public String getGetCustomPropertyFromAnotherFile() {
+    return customPropertyFromAnotherFile;
+  }
+
+  public String getCustomProperty() {
+    return customProperty;
+  }
+
+  public Integer getCustomPropertyInt() {
+    return customerPropertyInt;
+  }
 }
