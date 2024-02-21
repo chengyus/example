ae2a1a1ba632c5cbac20ab5449399e80d71668d5
Joseph Sung <chengyu.sung@gmail.com>
2024-02-21 21:15:19 +0800

    up to around 2:24:40; IDEA's auto import feature is definitely really good. HttpStatus import fix.

--- src/main/java/com/alibou/example/FirstController.java
+++ src/main/java/com/alibou/example/FirstController.java
@@ -1,13 +1,23 @@
 package com.alibou.example;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 
 @RestController
 public class FirstController {
   
-  @GetMapping
-  public String SayHello() {
+  @GetMapping("/hello")
+  public String sayHello() {
     return "Hello from my first controller";
   }
+  
+  @GetMapping("/hello-2")
+  @ResponseStatus(HttpStatus.ACCEPTED)
+  public String sayHello2() {
+    return "Hello 2 from my first controller";
+  }
+  
 }
