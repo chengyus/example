238eecabcecdd35ac525969c2041ac11c694d14f
Joseph Sung <chengyu.sung@gmail.com>
2024-02-21 20:17:26 +0800

    followed video for REST part up to around 2:18:33; had to learn to run ./mvnw clean ; then it runs.

--- src/main/java/com/alibou/example/ExampleApplication.java
+++ src/main/java/com/alibou/example/ExampleApplication.java
@@ -3,23 +3,11 @@ package com.alibou.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		var app = new SpringApplication(ExampleApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "test"));
-		var ctx = app.run(args);
-
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellAStory());
-		System.out.println(myFirstService.getCustomProperty());
-		System.out.println(myFirstService.getCustomPropertyInt());
-		System.out.println(myFirstService.getCustomPropertyFromAnotherFile());
-		System.out.println(myFirstService.getCustomPropertyFromAnotherFile2());
-
+		SpringApplication.run(ExampleApplication.class);
 	}
 
 }
