c7aec1a8237b667612e4ab2730888b9eb650af8d
Joseph Sung <chengyu.sung@gmail.com>
2024-02-21 13:00:46 +0800

    commit from cygwin shell; saving all these profile related changes; I trial-installed and uninstall the IDEA Ultimate version. It's pretty bad they limited sprint boot to be in Ultimate version only. But I saw they have appliation config that allow env; which is fine i think. keeping all these programatic changes.

--- src/main/java/com/alibou/example/ApplicationConfig.java
+++ src/main/java/com/alibou/example/ApplicationConfig.java
@@ -1,9 +1,7 @@
 package com.alibou.example;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApplicationConfig {
