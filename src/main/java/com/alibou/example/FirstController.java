6a323e2f5d1c0215a9e94ac35caf633b8b06e0a2
Joseph Sung <chengyu.sung@gmail.com>
2024-02-22 14:33:00 +0800

    up to postman and RequestBody

--- src/main/java/com/alibou/example/FirstController.java
+++ src/main/java/com/alibou/example/FirstController.java
@@ -1,9 +1,7 @@
 package com.alibou.example;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -14,10 +12,10 @@ public class FirstController {
     return "Hello from my first controller";
   }
   
-  @GetMapping("/hello-2")
-  @ResponseStatus(HttpStatus.ACCEPTED)
-  public String sayHello2() {
-    return "Hello 2 from my first controller";
+  @PostMapping("/post")
+  public String post(@RequestBody String message) {
+    return "Request accepted and message is : " + message;
   }
+
   
 }
