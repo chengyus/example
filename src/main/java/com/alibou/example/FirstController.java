58358845712c92452dd16bf835d6b5f70cea45ca
Joseph Sung <chengyu.sung@gmail.com>
2024-02-22 19:21:50 +0800

    PostMapping and that new endpoint /post-order; and new Order class; I also spend some time really enabling nvim-qt and nvim for nvim-java plugin IDE in nvim support; yep; all that spring boot annotations works thru the LSP and the nvim and lazyvim magic.

--- src/main/java/com/alibou/example/FirstController.java
+++ src/main/java/com/alibou/example/FirstController.java
@@ -4,14 +4,19 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirstController {
-  
+
   @GetMapping("/hello")
   public String sayHello() {
     return "Hello from my first controller";
   }
-  
+
   @PostMapping("/post")
   public String post(@RequestBody String message) {
     return "Request accepted and message is : " + message;
+  }
+
+  @PostMapping("/post-order")
+  public String post(@RequestBody Order order) {
+    return "Request accepted and message is : " + order.toString();
   }
 }
