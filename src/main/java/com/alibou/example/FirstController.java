648e76b5aac4f56bc36d3ce5a05aeb573c78001c
Joseph Sung <chengyu.sung@gmail.com>
2024-03-01 08:53:20 +0800

    cleanup

--- src/main/java/com/alibou/example/FirstController.java
+++ src/main/java/com/alibou/example/FirstController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class FirstController {
 
-  // @GetMapping("/hello")
+  @GetMapping("/hello")
   public String sayHello() {
     return "Hello from my first controller";
   }
@@ -13,27 +13,5 @@ public class FirstController {
   @PostMapping("/post")
   public String post(@RequestBody String message) {
     return "Request accepted and message is : " + message;
-  }
-
-  @PostMapping("/post-order")
-  public String post(@RequestBody Order order) {
-    return "Request accepted and message is : " + order.toString();
-  }
-
-  @PostMapping("/post-order-record")
-  public String postRecord(@RequestBody OrderRecord order) {
-    return "Request accepted and message is : " + order.toString();
-  }
-
-  @GetMapping("/hello/{user-name}")
-  public String pathVar(@PathVariable("user-name") String userName) {
-    return "my value = " + userName;
-  }
-
-  @GetMapping("/hello")
-  public String paramVar(
-      @RequestParam("user-name") String userName,
-      @RequestParam("user-lastname") String userLastname) {
-    return "my value = " + userName + "   " + userLastname;
   }
 }
