1a46d7d30e0dac95f883f70818a12fd3098c1503
Joseph Sung <chengyu.sung@gmail.com>
2024-03-05 13:51:07 +0800

    Get all and Get by Id implemented for Student

--- src/main/java/com/alibou/example/FirstController.java
+++ src/main/java/com/alibou/example/FirstController.java
@@ -2,6 +2,8 @@ package com.alibou.example;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class FirstController {
 
@@ -14,5 +16,14 @@ public class FirstController {
   @PostMapping("/students")
   public Student post(@RequestBody Student student) {
     return repository.save(student);
+  }
+
+  @GetMapping("/students")
+  public List<Student> findAllStudent() {
+    return repository.findAll();
+  }
+  @GetMapping("/students/{student-id}")
+  public Student findStudentById(@PathVariable("student-id") Integer id) {
+    return repository.findById(id).orElse(new Student());
   }
 }
