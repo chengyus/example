a732f34b3606ca2ba00203b9dde51912ec5886a8
Joseph Sung <chengyu.sung@gmail.com>
2024-03-05 16:32:25 +0800

    Search under GetMapping and Deletemapping and changes to StudentRepository supporting these.

--- src/main/java/com/alibou/example/FirstController.java
+++ src/main/java/com/alibou/example/FirstController.java
@@ -1,5 +1,6 @@
 package com.alibou.example;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +23,22 @@ public class FirstController {
   public List<Student> findAllStudent() {
     return repository.findAll();
   }
+
   @GetMapping("/students/{student-id}")
   public Student findStudentById(@PathVariable("student-id") Integer id) {
     return repository.findById(id).orElse(new Student());
+  }
+
+  @GetMapping("/students/search/{student-name}")
+  public List<Student> findStudentsByName(@PathVariable("student-name") String name) {
+    return repository.findAllByFirstnameContaining(name);
+  }
+
+  @DeleteMapping("/students/{student-id}")
+  @ResponseStatus(HttpStatus.OK)
+  public void delete(
+    @PathVariable("student-id") Integer id
+  ) {
+    repository.deleteById(id);
   }
 }
