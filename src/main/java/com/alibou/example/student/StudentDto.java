a09f3095ccb8818d928f4a51ccb7939b02b38660
Joseph Sung <chengyu.sung@gmail.com>
2024-04-10 13:09:25 +0800

    updated code pom.xml StudentController, StudentDto to handle exception of Student data entering and reflecting errors in the response JSON (up to video 6:11:10).

--- src/main/java/com/alibou/example/student/StudentDto.java
+++ src/main/java/com/alibou/example/student/StudentDto.java
@@ -1,7 +1,11 @@
 package com.alibou.example.student;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record StudentDto(
+    @NotEmpty
     String firstname,
+    @NotEmpty
     String lastname,
     String email,
     Integer schoolId) {
